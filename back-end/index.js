7af4d1e08096a895be1727567ff373581a205378
airpiazza <nick@piazzaonline.com>
2021-08-08 08:41:44 -0400

    connect to db

--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 const PORT = 3000;
+
+mongoose.connect("mongodb://localhost:27017/spesaDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     res.send(`This app is running on port ${PORT}`);
